521b62c295cec6f379a55c6c8e89626c288f38b9
Dominic Butler <domb5041@icloud.com>
2019-04-22 10:09:07 +0100

    new spotlight effect

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@
 			<div
 				v-for="(project, index) in projects"
 				:key="index"
-				class="project"
+				class="project hidden"
 				:id="'project-' + index "
-				@click="clickProject($event)"
-				@mousemove="mousePosition($event)"
+				@click="clickProject($event, index)"
+				@mousemove="mousePosition($event, index)"
+				@mouseenter="mouseEntered($event)"
+				@mouseleave="mouseLeft($event)"
 			>
-				<div
-					class="inner"
-					:style="`background: radial-gradient(at ${panda.x}px ${panda.y}px, azure, darkslategrey)`"
-				>
+				<div class="inner">
+					<div class="shroud" :style="`left: ${project.spotlight.x}px; top: ${project.spotlight.y}px;`"></div>
 					<img class="image" :src="imgUrl(project.image)" :alt="project.description">
 					<div class="details">
 						<p class="text" v-html="project.description"></p>
@@ -35,7 +35,7 @@
 			imgUrl: function(path) {
 				return require(`./assets/${path}`);
 			},
-			clickProject(event) {
+			clickProject(event, index) {
 				let newId = event.currentTarget.id;
 				let lastId = this.lastActiveId;
 				let newActive = document.getElementById(newId);
@@ -48,105 +48,125 @@
 
 				this.lastActiveId = newId;
 
-				this.mousePosition(event);
+				this.mousePosition(event, index);
 			},
-			mousePosition(event) {
+			mouseEntered(event) {
+				document
+					.getElementById(event.currentTarget.id)
+					.classList.remove("hidden");
+			},
+			mouseLeft(event) {
+				document
+					.getElementById(event.currentTarget.id)
+					.classList.add("hidden");
+			},
+			mousePosition(event, index) {
 				let element = document
 					.getElementById(event.currentTarget.id)
 					.getBoundingClientRect();
 
-				if (event.currentTarget.id == this.lastActiveId) {
-					this.panda.x = parseFloat(
-						event.pageX - (element.left + window.scrollX + 40)
-					);
-					this.panda.y = parseFloat(
-						event.pageY - (element.top + window.scrollY + 40)
-					);
-				}
+				this.projects[index].spotlight.x = parseFloat(
+					event.pageX - (element.left + window.scrollX + 40)
+				);
+				this.projects[index].spotlight.y = parseFloat(
+					event.pageY - (element.top + window.scrollY + 40)
+				);
 			}
 		},
 		data() {
 			return {
 				lastActiveId: "project-0",
-				panda: { x: 0, y: 0 },
 				projects: [
 					{
 						description:
 							"I'm a <span>frontend developer</span> and <span>designer</span>. You can find my recent work on this page, or <a href='Dominic-Butler-CV.pdf' target='blank'>click here</a> to open my CV.",
-						image: "avatar.jpg"
+						image: "avatar.jpg",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"A website for a small digital agency, built with <span>Vue</span>, <span>Nuxt</span> and <span>UI Kit</span>.",
 						image: "discoverit.jpg",
-						link: "https://discoverit.digital"
+						link: "https://discoverit.digital",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"Guitar theory made bright and lively with <span>Illustrator</span> and <span>After Effects</span>",
 						image: "notecircle.jpg",
 						link:
-							"https://www.youtube.com/watch?v=JChDbIOkE58&list=PLrRbyFPS5q6w8wLmKAudLnPg1L7CMv1Lw"
+							"https://www.youtube.com/watch?v=JChDbIOkE58&list=PLrRbyFPS5q6w8wLmKAudLnPg1L7CMv1Lw",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"A collection of my <span>graphic design</span> and illustration work.",
 						image: "teddy.jpg",
-						link: "https://www.instagram.com/dombutlerpad/"
+						link: "https://www.instagram.com/dombutlerpad/",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"An <span>interactive visualisation</span> showing the scale of planets in our solarsystem. Built with <span>D3</span>",
 						image: "planets.jpg",
-						link: "https://planet-scale.herokuapp.com"
+						link: "https://planet-scale.herokuapp.com",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"A model driven colour palette built with <span>React</span>.",
 						image: "colourPalette.png",
-						link: "https://material-colour-palette.herokuapp.com"
+						link: "https://material-colour-palette.herokuapp.com",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"Visualising progress through the year down to the second. Built with <span>D3</span>",
 						image: "barclock.jpg",
-						link: "https://bar-clock.herokuapp.com"
+						link: "https://bar-clock.herokuapp.com",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"Connecting to Kentico Cloud API. built with <span>React</span>.",
 						image: "cafes.jpg",
-						link: "https://kentico-cafes.herokuapp.com/"
+						link: "https://kentico-cafes.herokuapp.com/",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"A simple <span>React</span> app for recording statistical data during a race.",
 						image: "runners.png",
-						link: "https://dcsl-runners.herokuapp.com"
+						link: "https://dcsl-runners.herokuapp.com",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"A <span>Javascript</span> clock that changes colour.",
 						image: "colourclock.jpg",
-						link: "https://colour-clock.herokuapp.com"
+						link: "https://colour-clock.herokuapp.com",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"Experimenting with animation in <span>Javascript</span>",
 						image: "helloworld.jpg",
-						link: "https://hello-world-animation.herokuapp.com"
+						link: "https://hello-world-animation.herokuapp.com",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"MonoNote is minimalist note-pad app built with <span>React</span> and <span>Bootstrap</span>",
 						image: "mononote.jpg",
-						link: "https://mononote.herokuapp.com/"
+						link: "https://mononote.herokuapp.com/",
+						spotlight: { x: 0, y: 0 }
 					},
 					{
 						description:
 							"A simple <span>Javascript</span> memory game using the checkbox element.",
 						image: "checkboxes.png",
-						link: "https://checkbox-challenge.herokuapp.com"
+						link: "https://checkbox-challenge.herokuapp.com",
+						spotlight: { x: 0, y: 0 }
 					}
 				]
 			};
@@ -191,6 +211,9 @@
 		@media (max-width: @small-screen) {
 			padding: 40px 0;
 		}
+		&.hidden .shroud {
+			opacity: 0;
+		}
 	}
 
 	.project .inner {
@@ -201,10 +224,25 @@
 		border-radius: 5px;
 		transition: 0.2s;
 		opacity: 0.3;
+		background: rgba(255, 255, 255, 0.4);
 		@media (max-width: @small-screen) {
 			width: 95vw;
 			height: 95vw;
 			opacity: 1;
+		}
+	}
+
+	.inner .shroud {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 1160px;
+		height: 1160px;
+		transition: opacity 0.4s;
+		background: radial-gradient(at center, transparent, black);
+		transform: translate(-580px, -580px);
+		@media (max-width: @small-screen) {
+			display: none;
 		}
 	}
 
@@ -224,7 +262,7 @@
 		opacity: 0;
 		width: 100%;
 		height: 100%;
-		background-color: rgba(0, 0, 0, 0.3);
+		background-color: rgba(0, 0, 0, 0.1);
 		display: flex;
 		justify-content: center;
 		align-items: center;
@@ -280,10 +318,10 @@
 			opacity: 1;
 			transform: scale(1.03);
 			cursor: default;
-			// background: radial-gradient(at 60% 40px, white, silver, transparent);
+			background-color: darkslategray;
 			.image {
 				filter: blur(10px);
-				opacity: 0.6;
+				opacity: 0.5;
 				transform: scale(1.5);
 			}
 			.details {
